6ad7613b87e5254af8a198fb0d65753541a34af4
SHOTAiiMURA <hn.j.shota027503@gmail.com>
2024-06-09 14:15:36 +0900

    ageruno wasureteyo. forgive me. changed alot of codes already.

---
+++ To do app_exe/To-do-productivity/lambda_function.py
@@ -0,0 +1,324 @@
+import os
+import sys
+import logging
+import requests
+import urllib
+import urllib.request
+import json
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+from linebot.models import RichMenu, RichMenuArea, RichMenuBounds, RichMenuSize, URIAction
+from linebot.models import CameraAction, CameraRollAction
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URITemplateAction,
+    PostbackTemplateAction, DatetimePickerTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage, ImageSendMessage, VideoSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent, FlexSendMessage
+)
+from linebot.models.actions import PostbackAction
+
+from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
+from sql import insert_json_chooseDuration, choose_duration, choose_priority, insert_hour_duration
+import pymysql.cursors
+
+conn = pymysql.connect(host='tododbpy.c94g44mqus56.ap-northeast-1.rds.amazonaws.com',
+                    user='admin',
+                    password = '1qaz2wsx',
+                    db='Tododb',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+# LINEBOTと接続するための記述
+# 環境変数からLINEBotのチャンネルアクセストークンとシークレットを読み込む
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+# 無いならエラー
+if channel_secret is None:
+    logger.error('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    logger.error('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+# apiとhandlerの生成（チャンネルアクセストークンとシークレットを渡す）
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+shota_id = "U216e9f3bb4781422a3d3af51e0697dd3"
+
+
+# Lambdaのメインの動作
+def lambda_handler(event, context):
+    print(event)
+    # 認証用のx-line-signatureヘッダー
+    signature = event["headers"]["x-line-signature"]
+    body = event["body"]
+
+    # リターン値の設定
+    ok_json = {"isBase64Encoded": False,
+               "statusCode": 200,
+               "headers": {},
+               "body": ""}
+    error_json = {"isBase64Encoded": False,
+                  "statusCode": 500,
+                  "headers": {},
+                  "body": "Error"}
+
+    # 例外処理としての動作
+    try:
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        logger.error("Got exception from LINE Messaging API: %s\n" % e.handle_message)
+        for m in e.error.details:
+            logger.error("  %s: %s" % (m.property, m.handle_message))
+        return error_json
+    except InvalidSignatureError:
+        return error_json
+
+    return ok_json
+
+
+# 以下でWebhookから送られてきたイベントをどのように処理するかを記述する
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    send_message = event.message.text
+    print(event)
+    display_name = 'None'
+    if isinstance(event.source, SourceUser):
+        profile = line_bot_api.get_profile(event.source.user_id)
+        user_id = event.source.user_id
+        display_name = profile.display_name
+
+    else:
+        print("user profile can't not use")
+
+    if send_message == "New Task" and isinstance(event.source, SourceUser):
+        bubble_string = """{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "New Task",
+            "weight": "bold",
+            "color": "#555555",
+            "align": "center",
+            "size": "xl"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [],
+                "height": "1px",
+                "backgroundColor": "#aaaaaa",
+                "offsetTop": "7px"
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [],
+                "position": "absolute",
+                "width": "0%",
+                "height": "3px",
+                "backgroundColor": "#aaaaaa",
+                "offsetTop": "6px"
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [],
+                    "width": "14px",
+                    "height": "14px",
+                    "backgroundColor": "#08C656",
+                    "cornerRadius": "7px"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [],
+                    "width": "10px",
+                    "height": "10px",
+                    "backgroundColor": "#aaaaaa",
+                    "cornerRadius": "5px"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [],
+                    "width": "10px",
+                    "height": "10px",
+                    "backgroundColor": "#aaaaaa",
+                    "cornerRadius": "5px"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [],
+                    "width": "10px",
+                    "height": "10px",
+                    "backgroundColor": "#aaaaaa",
+                    "cornerRadius": "5px"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [],
+                    "width": "10px",
+                    "height": "10px",
+                    "backgroundColor": "#aaaaaa",
+                    "cornerRadius": "5px"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [],
+                    "width": "10px",
+                    "height": "10px",
+                    "backgroundColor": "#aaaaaa",
+                    "cornerRadius": "5px"
+                  }
+                ],
+                "position": "absolute",
+                "width": "100%",
+                "justifyContent": "space-between",
+                "alignItems": "center"
+              }
+            ],
+            "height": "14px"
+          },
+          {
+            "type": "separator"
+          }
+        ],
+        "spacing": "lg"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Type task before title yours",
+            "size": "xl",
+            "color": "#555555"
+          },
+          {
+            "type": "text",
+            "text": "Ex) Task learn SQL",
+            "color": "#aaaaaa"
+          }
+        ],
+        "spacing": "md"
+      }
+    ],
+    "spacing": "xl"
+  }
+}
+"""
+        message = FlexSendMessage(alt_text="Type Your Task", contents=json.loads(bubble_string))
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+        )
+    elif send_message[:5] == "Task " and isinstance(event.source, SourceUser):
+        #ex) send_message = "Task learn SQL"
+        user_task = send_message.replace("Task ","")
+        #user_task = "learn SQL"
+        if user_task.isalpha():
+            bubble = insert_json_chooseDuration(user_task)
+        message = FlexSendMessage(alt_text="Type Your Task", contents= bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+        )
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data[-len("Duration + Date"):] == 'Duration + Date':
+        line_bot_api.reply_message(
+            event.reply_token,
+            (TextSendMessage(text='Duration + Date'))
+        )
+    elif event.postback.data[-len("Duration"):] == 'Duration':
+        data_list = event.postback.data.split(",")
+        title = data_list[0]
+        bubble =  insert_hour_duration(title)
+        message = FlexSendMessage(alt_text="Choose Minutes", contents=bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+        )
+    elif event.postback.data[-len("Duration + Date"):] == 'Start + End':
+        line_bot_api.reply_message(
+            event.reply_token,
+            (TextSendMessage(text='Start + End'))
+        )
+    elif event.postback.data[-len(" hour")] == " hour":
+        dataList = event.postback.data.split(",")
+        name = dataList[0]
+        hour_duraiton = dataList[1].replace(" hour", "")
+        # hour_duration = 1 or other seleciton.
+        if hour_duraiton.isdigit():
+            bubble = choose_duration(name, hour_duraiton)
+
+            message = FlexSendMessage(alt_text="Choose Minutes", contents=bubble)
+            line_bot_api.reply_message(
+                event.reply_token,
+                message
+            )
+    elif event.postback.data[-len(" mins")] == " mins":
+        dataList = event.postback.data.split(",")
+        name = dataList[0]
+        hour_duraiton = dataList[1].replace(" hour", "")
+        minutes_duraiton = dataList[2].replace(" mins", "")
+        # hour_duration = 1 or other seleciton.
+        if hour_duraiton.isdigit():
+            bubble = choose_priority(name, hour_duraiton,minutes_duraiton)
+
+            message = FlexSendMessage(alt_text="Choose your priority", contents=bubble)
+            line_bot_api.reply_message(
+                event.reply_token,
+                message
+            )
+
+    # return priority
+    elif event.postback.data == '0 minutes' or '15 minutes' or '25 minutes' or '45 minutes':
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            (TextSendMessage(text='Choose Priority of Task'))
+        )
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(
+            text='登録して頂きありがとうごいます！Cafe BORCELLEのLINE公式アカウントです。\n\n下記Menuから今日のドリンクメニューやクーポン、お問い合わせ、ホームページのリンクがご確認頂けます。\n\n自動会話botと連携しており、チャットで話しかけると返答が返ってきます。\n\nぜひご活用頂ければ幸いです。'))
---
+++ To do app_exe/To-do-productivity/mysql_database/TodoSql.py
@@ -0,0 +1,151 @@
+import pymysql.cursors
+
+conn = pymysql.connect(host='tododbpy.c94g44mqus56.ap-northeast-1.rds.amazonaws.com',
+                    user='admin',
+                    password = '1qaz2wsx',
+                    db='Tododb',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
+try:
+    conn.ping(reconnect=True)  # Reconnect if necessary
+    print("Connection is active.")
+except Exception as e:
+    print(f"Error: {e}")
+def my_db(conn):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS my_db.Priority (
+  priority VARCHAR(10) NOT NULL,
+  PRIMARY KEY (priority),
+  UNIQUE INDEX priority_UNIQUE (priority ASC) VISIBLE)
+ENGINE = InnoDB;
+                """
+            )
+            cur.execute(
+                """
+    CREATE TABLE IF NOT EXISTS my_db.User (
+      line_id VARCHAR(45) NOT NULL,
+      name VARCHAR(45) NULL,
+      PRIMARY KEY (line_id))
+    ENGINE = InnoDB;
+                """
+            )
+
+            cur.execute(
+                """
+    CREATE TABLE IF NOT EXISTS my_db.Task (
+      task_id INT NOT NULL AUTO_INCREMENT,
+      name VARCHAR(100) NULL,
+      duration INT NULL,
+      state CHAR(2) NOT NULL,
+      due_date DATETIME NULL,
+      end_time DATETIME NULL,
+      start_time DATETIME NULL,
+      extended_time INT NOT NULL DEFAULT 0,
+      priority VARCHAR(10) NULL,
+      line_id VARCHAR(45) NOT NULL,
+      PRIMARY KEY (task_id),
+      UNIQUE INDEX task_id_UNIQUE (task_id ASC) VISIBLE,
+      INDEX fk_Task_Priority1_idx (priority ASC) VISIBLE,
+      INDEX fk_Task_User1_idx (line_id ASC) VISIBLE,
+      CONSTRAINT fk_Task_Priority1
+        FOREIGN KEY (priority)
+        REFERENCES my_db.Priority (priority)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+      CONSTRAINT fk_Task_User1
+        FOREIGN KEY (line_id)
+        REFERENCES my_db.User (line_id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+                """
+            )
+            conn.commit()
+        except Exception as e:
+            print("Error")
+            raise ValueError(str(e))
+
+def show_tables(conn):
+    with conn.cursor() as cur:
+        try:
+            cur.execute("show tables;")
+
+            for row in list(cur):
+                # Try getting profile
+                print(row)
+            conn.commit()
+        except Exception as e:
+            raise ValueError(str(e))
+
+def insert_priorities(conn,priority):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(f"Insert ignore into Priority (priority) Values('{priority}');")
+            conn.commit()
+        except Exception as e:
+                raise ValueError(str(e))
+
+def select_priorities(conn):
+    with conn.cursor() as cur:
+        try:
+            cur.execute("select * from Priority;")
+
+            for row in list(cur):
+                # Try getting profile
+                print(row)
+            conn.commit()
+        except Exception as e:
+            raise ValueError(str(e))
+
+
+def drop_table(conn):
+    with conn.cursor() as cur:
+        try:
+            cur.execute("SET foreign_key_checks = 0;")
+            #cur.execute("Drop Table Priority")
+            #cur.execute("Drop Table Task")
+            #cur.execute("Drop Table User")
+            conn.commit()
+        except Exception as e:
+            raise ValueError(str(e))
+
+# ユーザーID取得 event.source.user_id
+user_id = input("enter your ID: ")
+
+# ユーザー情報取得
+with conn.cursor() as cursor:
+    cursor.execute("SELECT * FROM User WHERE line_id = %s", (user_id,))
+    user = cursor.fetchone()
+
+# ユーザーが存在しない場合は新規登録
+if user is None:
+    with conn.cursor() as cursor:
+        cursor.execute("INSERT INTO User (line_id) VALUES (%s)", (user_id,))
+        conn.commit()
+
+# ユーザー情報を更新
+#with conn.cursor() as cursor:
+ #   cursor.execute("UPDATE User SET name = %s WHERE line_id = %s", (event.message.text, user_id))
+#    conn.commit()
+
+# 更新されたユーザー情報取得
+#with conn.cursor() as cursor:
+ #   cursor.execute("SELECT * FROM User WHERE line_id = %s", (user_id,))
+ #   updated_user = cursor.fetchone()
+
+# 更新されたRowをプリント
+#print(updated_user)
+
+for row in user_id:
+    print(row)
+
+# データベース接続を閉じる
+conn.close()
+
+select_priorities(conn)
+insert_priorities(conn,"high")
+insert_priorities(conn,"medium")
+insert_priorities(conn,"low")
