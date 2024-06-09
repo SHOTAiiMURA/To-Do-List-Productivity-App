72a9172b5b051a487e37f8acfa29d3eb3e56fd10
SHOTAiiMURA <hn.j.shota027503@gmail.com>
2024-06-09 19:57:26 +0900

    modify funcitons

--- To do app_exe/To-do-productivity/lambda_function.py
+++ To do app_exe/To-do-productivity/lambda_function.py
@@ -330,8 +330,6 @@ def handle_postback(event):
         minutes_duraiton = dataList[2].replace(" mins", "")
         priority = dataList[3]
 
-        bubble = task_created(name, hour_duraiton, minutes_duraiton, priority)
-
         create_task(conn,name,hour_duraiton,minutes_duraiton,priority,event.source.user_id)
 
         line_bot_api.reply_message(
